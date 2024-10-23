6153fd9f294756e48d9c481c13b3170a906d71df
Pedro Sandrini <sandrini.pedro@outlook.com>
2024-10-23 19:03:40 -0300

    Add default user, and alter templates

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,20 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    tags = models.ManyToManyField('Tag', related_name='posts')
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Tag(models.Model):
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- users/apps.py
+++ users/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class UsersConfig(AppConfig):
@@ -6,4 +7,6 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        import users.signals
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin')
